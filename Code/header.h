5149340fdd833af7a08e3b931cd67ccae94a5122
haxkadc <134702013+haxkadc@users.noreply.github.com>
2023-05-26 20:10:40 +0200

    Create header.h

---
+++ Code/header.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define RIGHE 4
+#define COLONNE 8
+
+void formattazione(char testo_entrata[], char testo_uscita[],char mat_sost[][8]);
+
+void cifratura(char *, char *,char mat_sost[][8]);
+
+void decifratura(char *, char *,char mat_sost[][8]);
+
